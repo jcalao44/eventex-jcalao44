96e5111b6f0ec150647baddd6857e7e1465421e8
jcalao44 <jcalao44@gmail.com>
2021-12-11 18:17:07 -0300

    Realiza inscrição (console)

--- eventex/settings.py
+++ eventex/settings.py
@@ -125,4 +125,13 @@ STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email configuration
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL__PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+"""
