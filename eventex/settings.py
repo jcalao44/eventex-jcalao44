7bb42e5ec8423c507df192ab08657e1350f24f3b
jcalao44 <jcalao44@gmail.com>
2021-12-11 20:35:09 -0300

    From alinhado com o Sender Identity

--- eventex/settings.py
+++ eventex/settings.py
@@ -126,12 +126,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email configuration
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-"""
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL__PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-"""
